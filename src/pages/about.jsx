ac25c89a5b1e51cd9808ff0c5a8f23fcc0b117ae
Andreas Siedler <andreas.siedler@gmail.com>
2023-04-07 13:31:39 +0200

    Add phone number to about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -12,11 +12,11 @@ import {
 } from '@/components/SocialIcons'
 import portraitImage from '@/images/andreas-siedler-portrait.jpg'
 
-function SocialLink({ className, href, children, icon: Icon }) {
+function SocialLink({ className, target, href, children, icon: Icon }) {
   return (
     <li className={clsx(className, 'flex')}>
       <Link
-        target="_blank"
+        target={target}
         href={href}
         className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
       >
@@ -33,6 +33,19 @@ function MailIcon(props) {
       <path
         fillRule="evenodd"
         d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+      />
+    </svg>
+  )
+}
+
+function PhoneIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <rect width="20" height="20" fill="white" />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M2.14089 4.4021C2.34025 2.92963 3.63956 2 4.99004 2H7.55848C8.84977 2 9.99619 2.82629 10.4045 4.05132L11.454 7.19963C11.905 8.55283 11.1086 10.0036 9.72482 10.3496C9.38646 10.4342 9.26975 10.8555 9.51637 11.1022L12.8978 14.4836C13.1445 14.7303 13.5658 14.6135 13.6504 14.2752C13.9964 12.8914 15.4472 12.095 16.8004 12.546L19.9487 13.5955C21.1737 14.0038 22 15.1502 22 16.4415V19.01C22 20.3604 21.0704 21.6598 19.5979 21.8591C18.9114 21.9521 18.211 22 17.5 22C8.93959 22 2 15.0604 2 6.5C2 5.78898 2.04794 5.08863 2.14089 4.4021Z"
       />
     </svg>
   )
@@ -112,6 +125,7 @@ export default function About() {
           <div className="lg:pl-20">
             <ul role="list">
               <SocialLink
+                target={'_blank'}
                 href="https://twitter.com/SiedlerAndreas"
                 icon={TwitterIcon}
               >
@@ -121,6 +135,7 @@ export default function About() {
                 Follow on Instagram
               </SocialLink> */}
               <SocialLink
+                target={'_blank'}
                 href="https://github.com/AndreasSiedler"
                 icon={GitHubIcon}
                 className="mt-4"
@@ -128,6 +143,7 @@ export default function About() {
                 Follow on GitHub
               </SocialLink>
               <SocialLink
+                target={'_blank'}
                 href="https://www.linkedin.com/in/andreas-siedler-8415a8b8/"
                 icon={LinkedInIcon}
                 className="mt-4"
@@ -135,11 +151,19 @@ export default function About() {
                 Follow on LinkedIn
               </SocialLink>
               <SocialLink
+                target={'_blank'}
                 href="mailto:andreas.siedler@gmail.com"
                 icon={MailIcon}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
                 andreas.siedler@gmail.com
+              </SocialLink>
+              <SocialLink
+                href="tel:+436769457566"
+                icon={PhoneIcon}
+                className="mt-4"
+              >
+                +43 676 945 75 66
               </SocialLink>
             </ul>
           </div>
