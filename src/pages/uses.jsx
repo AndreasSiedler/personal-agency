e039a84082e21b7ef710569fc879f38887abaadf
Andreas Siedler <andreas.siedler@gmail.com>
2023-04-07 13:59:16 +0200

    Remove chair from uses page

--- src/pages/uses.jsx
+++ src/pages/uses.jsx
@@ -62,10 +62,10 @@ export default function Uses() {
               special powers. I really like feeling like a wizard with special
               powers.
             </Tool>
-            <Tool title="Herman Miller Aeron Chair">
+            {/* <Tool title="Herman Miller Aeron Chair">
               If I’m going to slouch in the worst ergonomic position imaginable
               all day, I might as well do it in an expensive chair.
-            </Tool>
+            </Tool> */}
           </ToolsSection>
           <ToolsSection title="Development tools">
             <Tool title="Sublime Text 4">
